e3b0471184c26097f4e333b897cc9a0866e9a119
MISTERSOFT <sofiane.hamadi.pro@gmail.com>
2018-06-08 14:25:38 +0200

    Update user data using Express path api

--- client/src/app/chat/menu/options-popup/options.service.ts
+++ client/src/app/chat/menu/options-popup/options.service.ts
@@ -1,13 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { WebSocketService } from '@core/websocket.service';
+import { HttpService } from '@core/http.service';
 import { User } from '@shared/models';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
-export class OptionsService extends WebSocketService {
-  constructor() {
+export class OptionsService extends HttpService {
+  constructor(private http: HttpClient) {
     super();
   }
-  updateProfile(user: User, handleResponse: Function) {
-    this.emit('UPDATE_USER', user, handleResponse);
+  updateProfile(user: User) {
+    const body = JSON.stringify(user);
+    return this.http.put(`${this.api}/user`, body, this.options)
+      .pipe(catchError(this.handleError));
   }
 }
